c22c1262a5436bbf92f61d5ee937aaab667ef5f3
Luciano Vernaschi <luciano@cromoteca.com>
2023-04-16 14:20:35 +0200

    Endpoint with subtypes

---
+++ webapp/src/main/java/com/example/application/endpoints/helloreact/SubtypesEndpoint.java
@@ -0,0 +1,38 @@
+package com.example.application.endpoints.helloreact;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import dev.hilla.Endpoint;
+import dev.hilla.Nonnull;
+import reactor.core.publisher.Flux;
+
+@Endpoint
+@AnonymousAllowed
+public class SubtypesEndpoint {
+  public Flux<@Nonnull AbstractEvent> subscribe() {
+    return Flux.just(new AddEvent(), new UpdateEvent(), new DeleteEvent());
+  }
+
+  public @Nonnull AbstractEvent single() {
+    return new AddEvent();
+  }
+
+  @JsonTypeInfo(use = JsonTypeInfo.Id.MINIMAL_CLASS)
+  public static class AbstractEvent {
+    public String name;
+  }
+
+  public static class AddEvent extends AbstractEvent {
+    public String item;
+  }
+
+  public static class UpdateEvent extends AbstractEvent {
+    public String oldItem;
+    public String newItem;
+  }
+
+  public static class DeleteEvent extends AbstractEvent {
+    public String item;
+    public boolean force;
+  }
+}
