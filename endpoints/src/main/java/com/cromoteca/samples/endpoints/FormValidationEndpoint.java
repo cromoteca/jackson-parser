64b7aa9f1c54379f8db30801053264f64b6fef32
Luciano Vernaschi <luciano@cromoteca.com>
2023-04-23 10:22:15 +0200

    Move from Formik to React Hook Form

--- endpoints/src/main/java/com/cromoteca/samples/endpoints/FormValidationEndpoint.java
+++ endpoints/src/main/java/com/cromoteca/samples/endpoints/FormValidationEndpoint.java
@@ -3,12 +3,23 @@ package com.cromoteca.samples.endpoints;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.Endpoint;
 import dev.hilla.Nonnull;
+import jakarta.validation.Constraint;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.Payload;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.List;
+import org.springframework.lang.Nullable;
 
 @Endpoint
 @AnonymousAllowed
@@ -19,11 +30,34 @@ public class FormValidationEndpoint {
     return "Registration accepted";
   }
 
+  @Documented
+  @Constraint(validatedBy = NotUsedBeforeValidator.class)
+  @Target({ElementType.FIELD})
+  @Retention(RetentionPolicy.RUNTIME)
+  public @interface NotUsedBefore {
+    String message() default "Already used";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+  }
+
+  public static class NotUsedBeforeValidator implements ConstraintValidator<NotUsedBefore, String> {
+    private static final List<String> USED_EMAILS = List.of("john.doe@example.com");
+    private static final List<String> USED_PHONES = List.of("0123456789");
+
+    public void initialize(NotUsedBefore constraintAnnotation) {}
+
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+      return value == null || !(USED_EMAILS.contains(value) || USED_PHONES.contains(value));
+    }
+  }
+
   @Valid
   public static record RegistrationInfo(
       @NotBlank String name,
-      @NotBlank @Email String email,
-      @Pattern(regexp = "^[0-9]+$") String phone,
+      @NotBlank @Email @NotUsedBefore String email,
+      @Nullable @Pattern(regexp = "^[0-9]*$") @NotUsedBefore String phone,
       @Size(min = 2, max = 3) String country,
-      @AssertTrue boolean conditions) {}
+      @AssertTrue boolean terms) {}
 }
--- webapp/frontend/views/helloform/HelloFormView.tsx
+++ webapp/frontend/views/helloform/HelloFormView.tsx
@@ -4,86 +4,47 @@ import { Notification } from '@hilla/react-components/Notification.js';
 import { Select } from '@hilla/react-components/Select.js';
 import { TextField } from '@hilla/react-components/TextField.js';
 import { VerticalLayout } from '@hilla/react-components/VerticalLayout.js';
+import { yupResolver } from '@hookform/resolvers/yup';
 import FormValidationEndpoint from 'Frontend/generated/com/cromoteca/samples/endpoints/FormValidationEndpoint.js';
 import RegistrationInfo from 'Frontend/generated/com/cromoteca/samples/endpoints/FormValidationEndpoint/RegistrationInfo.js';
-import { Field, Form, Formik } from 'formik';
+import { useForm } from 'react-hook-form';
 import { ObjectSchema, boolean, object, string } from 'yup';
 
 export default function HelloFormView() {
-    const countries = [
-        { value: "FI", label: "Finland" },
-        { value: "DE", label: "Germany" },
-        { value: "US", label: "United States" },
-    ];
+  const countries = [
+    { value: "FI", label: "Finland" },
+    { value: "DE", label: "Germany" },
+    { value: "US", label: "United States" },
+  ];
 
-    const initialValues: RegistrationInfo = {
-        name: '',
-        email: '',
-        phone: '',
-        country: '',
-        conditions: false,
-    };
+  const yupSchema: ObjectSchema<RegistrationInfo> = object({
+    name: string().required(),
+    email: string().email().required(),
+    phone: string().matches(/^[0-9]*$/).optional(),
+    country: string().min(2).max(3).required(),
+    terms: boolean().oneOf([true]).required(),
+  });
 
-    const yupSchema: ObjectSchema<RegistrationInfo> = object({
-        name: string().required(),
-        email: string().email().required(),
-        phone: string().matches(/^[0-9]+$/).required(),
-        country: string().min(2).max(2).required(),
-        conditions: boolean().oneOf([true]).required(),
-    });
+  const { register, handleSubmit, formState: { errors, isValid } } = useForm({
+    mode: 'all',
+    resolver: yupResolver(yupSchema),
+  });
 
-    return (
-        <Formik
-            initialValues={{ ...initialValues }}
-            validationSchema={yupSchema}
-            onSubmit={async values => {
-                const response = await FormValidationEndpoint.validate(values);
-                Notification.show(response, { theme: 'success' });
-            }}
-        >
-            {({ submitForm, isValid, dirty }) => (
-                <Form>
-                    <VerticalLayout theme="spacing padding">
-                        <Field name="name">
-                            {({ field, meta }: any) => (
-                                <TextField {...field} placeholder="Name"
-                                    invalid={meta.error && meta.touched} errorMessage={meta.error} />
-                            )}
-                        </Field>
+  const onSubmit = async (data: any) => {
+    const response = await FormValidationEndpoint.validate(data);
+    Notification.show(response, { theme: 'success' });
+  };
 
-                        <Field name="email">
-                            {({ field, meta }: any) => (
-                                <TextField {...field} placeholder="Email"
-                                    invalid={meta.error && meta.touched} errorMessage={meta.error} />
-                            )}
-                        </Field>
+  return (
+    <VerticalLayout theme="spacing padding">
+      <TextField label="Name" required {...register("name")} invalid={!!errors.name} errorMessage={`${errors.name?.message}`} />
+      <TextField label="Email" required {...register("email")} invalid={!!errors.email} errorMessage={`${errors.email?.message}`} />
+      <TextField label="Phone" {...register("phone")} invalid={!!errors.phone} errorMessage={`${errors.phone?.message}`} />
+      <Select label="Country" required {...register("country")} items={countries} invalid={!!errors.country} errorMessage={`${errors.country?.message}`} />
+      <Checkbox label="I accept terms and conditions" {...register("terms")} />
+      {errors.terms?.message && <p>{`${errors.terms?.message}`}</p>}
 
-                        <Field name="phone">
-                            {({ field, meta }: any) => (
-                                <TextField {...field} placeholder="Phone"
-                                    invalid={meta.error && meta.touched} errorMessage={meta.error} />
-                            )}
-                        </Field>
-
-                        <Field name="country">
-                            {({ field, meta }: any) => (
-                                <Select {...field} placeholder="Country" items={countries} name={field.name}
-                                    invalid={meta.error && meta.touched} errorMessage={meta.error} />
-                            )}
-                        </Field>
-
-                        <Field name="conditions">
-                            {({ field, meta }: any) =>
-                                <Checkbox {...field} label="I accept terms and conditions"
-                                    invalid={meta.error && meta.touched} errorMessage={meta.error}
-                                    onCheckedChanged={field.onChange} />
-                            }
-                        </Field>
-
-                        <Button theme="primary" onClick={submitForm} disabled={!(dirty && isValid)}>Submit</Button>
-                    </VerticalLayout>
-                </Form>
-            )}
-        </Formik>
-    );
+      <Button theme="primary" onClick={handleSubmit(onSubmit)} disabled={!isValid}>Submit</Button>
+    </VerticalLayout>
+  );
 }
