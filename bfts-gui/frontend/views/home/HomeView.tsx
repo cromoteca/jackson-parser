fe1f8378a721afeb8d4c6d6fe72ff7e563311977
Luciano Vernaschi <luciano@cromoteca.com>
2023-06-11 12:47:06 +0200

    Version updates

--- bfts-gui/frontend/views/home/HomeView.tsx
+++ bfts-gui/frontend/views/home/HomeView.tsx
@@ -1,13 +1,15 @@
 import Pair from "Frontend/generated/com/cromoteca/bfts/model/Pair";
-import { ConfigurationEndpoint } from "Frontend/generated/endpoints";
+import ConfigurationEndpoint from "Frontend/generated/com/example/application/endpoints/ConfigurationEndpoint";
+import ConnectedStorageConfiguration from "Frontend/generated/com/example/application/endpoints/ConfigurationEndpoint/ConnectedStorageConfiguration";
+import LocalStorageConfiguration from "Frontend/generated/com/example/application/endpoints/ConfigurationEndpoint/LocalStorageConfiguration";
 import { useEffect, useState } from "react";
 
 export default function HomeView() {
-    const [storages, setStorages] = useState<Pair>();
+    const [storages, setStorages] = useState<Pair<LocalStorageConfiguration[], ConnectedStorageConfiguration[]>>();
 
     useEffect(() => {
         ConfigurationEndpoint.listStorages().then(setStorages);
     }, []);
     
-    return <div>{storages?.first.}</div>;
+    return <div>{storages?.first?.length}</div>;
 }
